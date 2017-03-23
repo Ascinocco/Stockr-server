ba3fe7b78e81ecd11d9108c6ad6f55ad47380e6e
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-22 20:39:13 -0400

    setup CORS

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
+// routes
 var authRoutes = require('./api/routes/auth.routes');
 var userRoutes = require('./api/routes/user.routes');
 var stockRoutes = require('./api/routes/stock.routes');
+
+// middleware
+var CORSMiddleware = require('./api/middleware/cors.middleware');
 
 var app = express();
 
@@ -16,9 +21,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// routes
 app.use('/api/auth/', authRoutes);
 app.use('/api/users/', userRoutes);
 app.use('/api/stocks', stockRoutes)
+
+// my middleware
+app.use(cors());
+app.use(CORSMiddleware.allowCORS);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
