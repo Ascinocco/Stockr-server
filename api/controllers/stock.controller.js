e4c32f36b70ce11096a40f48f458a6c4d00238a7
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-24 23:40:28 -0400

    building out stock api

--- api/controllers/stock.controller.js
+++ api/controllers/stock.controller.js
@@ -1,35 +1,103 @@
 var express = require('express');
+var request = require('request');
+var Baby = require('babyparse');
 var User = require('../models/user');
 
 var StockController = (function() {
 
+    var basicFormat = [
+        's', 'n', 'a'
+    ];
+
     // returns all stocks a user is subscribed to
     var feed = function(req, res, next)
     {
-        res.json({
-            success: true,
-            msg: "Stock Routes form ctrl"
-        })
+       
     }
 
-    var search = function() {
+    var search = function(req, res, next) {
 
     }
 
-    var add = function() {
+    var add = function(req, res, next) {
 
     }
 
-    var remove = function() {
+    var remove = function(req, res, next) {
 
     }
 
     // realistically I should be getting all of the stocks and filtering based on revenue or market share or something
     // however the yahoo finance api makes it hard to do this and I don't have enough server horsepower to
     // so I googled to the most popular stocks and picked some and those will be the default
-    var popular = function() {
+    var popular = function(req, res, next) {
+        var symbols = [ 'AAPL', 'GOOG', 'GE', 'MSFT' ];
+        var url = buildStockQueryString(symbols, basicFormat);
+        var options = assembleRequest(url);
 
+        request(options, function(error, response, csvStockData) {
+            if (error) {
+                console.error('ERROR!!!---------');
+                console.error(error);
+            }
+            
+            var jsonStockData = Baby.parse(csvStockData, {
+                quotes: true,
+                quoteChar: '"',
+                delimiter: ',',
+                header: false,
+                newLine: '\n'
+            });
+
+            console.log(jsonStockData);
+
+            res.json({
+                success: true
+
+            })
+        });
     }
+
+    /**
+     * 
+     * @param {Array} symbols - array of stock symbols 
+     * @param {Array} formats - array of format options for yahoo finance api
+     */
+    var buildStockQueryString = function(symbols, formats) {
+        var url = "http://download.finance.yahoo.com/d/quotes.csv?";
+        var assembledSymbols = "s=";
+        var assembledFormats = "&f=";
+
+        for (var symbol = 0; symbol < symbols.length; symbol++) {
+            assembledSymbols += symbols[symbol].toString() + "+";
+        }
+
+        for (var format = 0; format < formats.length; format++) {
+            assembledFormats += formats[format].toString();
+        }
+
+        // remove + from the end of the assembled symbols
+        assembledSymbols = assembledSymbols.substring(0, assembledSymbols.length - 1);
+
+        // assemble complete URL
+        url += assembledSymbols + assembledFormats;
+
+        return url;
+    }
+
+    var assembleRequest = function(yahooFinanceQueryString) {
+
+        var options = {
+            url: yahooFinanceQueryString,
+            method: 'POST',
+        }
+
+        return options;
+    }
+
+   var resultArrayToJson = function(resultArray) {
+
+   }
 
     // functions and variables returned here are considered public
     return {
--- api/routes/stock.routes.js
+++ api/routes/stock.routes.js
@@ -9,7 +9,7 @@ router.use(authMiddlware.checkToken);
 router.post('/', stockController.feed); // get all stocks that user is subscribed to
 router.post('/search', stockController.search); // search for stocks
 router.post('/add', stockController.add); // add stock to watched stocks
-router.post('/popular')
+router.post('/popular', stockController.popular); // list of popular stocks
 router.delete('/remove', stockController.remove); // remove a followed stock
 
 module.exports = router;
